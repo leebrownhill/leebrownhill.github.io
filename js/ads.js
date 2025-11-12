c505f7c310851cc57021840f262049874d6b9a6c
Lee Brownhill <lbrownhill@live.co.uk>
2025-11-12 07:43:36 +0000

    deployed

---
+++ js/ads.js
@@ -0,0 +1,7 @@
+(function() {
+  var s = document.createElement('script');
+  s.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6435499912518262";
+  s.async = true;
+  s.crossOrigin = "anonymous";
+  document.head.appendChild(s);
+})();
